70213e3357bc38138d96f13319c30baca2fe6a88
Admin <Admin@DESKTOP-5TGJMDR>
2020-09-14 20:09:50 -0500

    Mi primera clase Main EDITADA

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/lddaza/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/lddaza/Main.java
@@ -10,5 +10,7 @@ package co.edu.sanmateo.codigofuente.lddaza;
  * @author Admin
  */
 public class Main {
-    
+   public static void main(String[] args) {
+        System.out.println("Hola Mundo Luz DAZA");
+    }
 }
