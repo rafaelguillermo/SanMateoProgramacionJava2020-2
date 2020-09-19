50d52b9f5bffd436e4cfe337d4fa847f01702a7f
yaq29 <yaq29@YAQ>
2020-09-18 23:16:43 -0500

    Iniciando.......

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Yuber07/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.sanmateo.codigofuente.Yuber07;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author yaq29
+ */
+public class Main {
+    
+     public static void main(String[] args) {
+       
+         JOptionPane.showMessageDialog(null, "Que se dice");
+    }
+   
+    
+}
