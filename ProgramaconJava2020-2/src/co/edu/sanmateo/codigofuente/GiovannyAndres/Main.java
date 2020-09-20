7142ee2b5c541796c458924be110ee185c261507
giovanny <gpatarroyo@sanmateo.edu.co>
2020-09-19 21:58:39 -0500

    Package, Giovanny Patarroyo

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/GiovannyAndres/Main.java
@@ -0,0 +1,17 @@
+/*
+                    FUNDACIÓN UNIVERSITARIA SAN MATEO
+                    FACULTAD DE INGENIERÍAS Y AFINES
+                        INGENIERÍA DE SISTEMAS
+                              SEMESTRE VI
+                                 2020
+ */
+package co.edu.sanmateo.codigofuente.GiovannyAndres;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class Main {
+     
+}
