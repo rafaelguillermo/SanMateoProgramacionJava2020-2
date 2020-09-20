62ffdcf94797e0b06cd8d37cd1381257fcf37d5b
User <User@DESKTOP-50LAFNC>
2020-09-20 17:34:37 -0500

    Mi primera clase en java

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/dgaitan861/main.java
@@ -0,0 +1,10 @@
+
+package co.edu.sanmateo.codigofuente.dgaitan861;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Raul Gaitan");
+    }
+    
+}
