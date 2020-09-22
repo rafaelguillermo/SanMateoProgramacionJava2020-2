0c29508278455c7ac18977bc94095916e826e23d
jrangelardila <jrangelardila@gmaill.com>
2020-09-21 23:14:27 -0500

    Se a realizado la clase main

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/madrigaly5/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/madrigaly5/Main.java
@@ -3,5 +3,8 @@ package co.edu.sanmateo.codigofuente.madrigaly5;
 
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println("este es un ejemplo");
+    }
     
 }
