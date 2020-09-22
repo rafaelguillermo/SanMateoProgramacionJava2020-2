3bbc3bcf91fbf590735a428ba7e058c5d919860d
yonie <yonie@DESKTOP-A4LKAFJ>
2020-09-21 19:53:02 -0500

    Creación de metodo Main, Universidad y modelo para el proyecto de ingreso de estudiantes

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/YcifuentesA/Estudiante.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.sanmateo.codigofuente.YcifuentesA;
+
+/**
+ *
+ * @author yonie
+ */
+public class Estudiante {
+    public String nombrecompleto;
+    public String cedula;
+    
+
+    public Estudiante(String nombrecompleto, String cedula) {
+        this.nombrecompleto = nombrecompleto;
+        this.cedula = cedula;
+    }
+    
+    
+    
+}
