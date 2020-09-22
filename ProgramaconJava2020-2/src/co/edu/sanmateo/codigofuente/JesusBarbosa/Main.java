375208a8f02ef44374c540ec75379758e43e72a8
LuckRotz <LuckRotz@DESKTOP-RHKO3D0>
2020-09-21 22:17:00 -0500

    Agregando Main

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
@@ -2,6 +2,6 @@ package co.edu.sanmateo.codigofuente.JesusBarbosa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Soy Jesus Barbosa");
+        System.out.println("Soy  Jesus Barbosa");
     }
 }
