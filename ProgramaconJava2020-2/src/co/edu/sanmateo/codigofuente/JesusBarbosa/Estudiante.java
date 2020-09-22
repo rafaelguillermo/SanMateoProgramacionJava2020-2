6fc8ac662589e6366162d068873f64c537bb459b
LuckRotz <LuckRotz@DESKTOP-RHKO3D0>
2020-09-21 22:34:34 -0500

    Agregando clases

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Estudiante.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.sanmateo.codigofuente.JesusBarbosa;
+
+/**
+ *
+ * @author LuckRotz
+ */
+public class Estudiante {
+    
+    public String nombreCompleto;
+    public String cedula;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String nombreCompleto, String cedula) {
+        this.nombreCompleto = nombreCompleto;
+        this.cedula = cedula;
+    }
+    
+    
+    
+}
