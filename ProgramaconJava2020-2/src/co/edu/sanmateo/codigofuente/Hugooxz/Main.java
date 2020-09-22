a4d86bfb855c5ae801ef8cf253e8b3ffd9bbc234
Administrador <Administrador@DESKTOP-75R47M6>
2020-09-21 21:41:28 -0500

    Codigo Fuente

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Hugooxz/Main.java
@@ -0,0 +1,17 @@
+
+package co.edu.sanmateo.codigofuente.Hugooxz;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    
+        
+     public static void main(String[] args) {
+       
+         JOptionPane.showMessageDialog(null, "activo mi carta trampa");{
+         
+     }
+     }
+}
+
+   
