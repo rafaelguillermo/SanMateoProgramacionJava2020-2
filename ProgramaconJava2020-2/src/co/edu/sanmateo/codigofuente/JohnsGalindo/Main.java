7c2861f4b1ad7f3490986ed00db959760dc7519c
John <John@LAPTOP-0Q1MI922>
2020-09-21 19:51:35 -0500

    Creando mi universidad con menu. Hasta ahora se crea el estudiante

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JohnsGalindo/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JohnsGalindo/Main.java
@@ -5,10 +5,37 @@
  */
 package co.edu.sanmateo.codigofuente.JohnsGalindo;
 
+import co.edu.sanmateo.codigofuente.JohnsGalindo.Modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.JohnsGalindo.Operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
 /**
  *
  * @author John
  */
 public class Main {
+    public static void main(String[] args) {
+       
+        Scanner entrada = new Scanner(System.in);
+        List<Estudiante> listaEstudiante = new ArrayList<>();
+        Universidad = new universidad = new Universidad();
+        
+        System.out.println("Bienvenido a la Universidad San Mateo");
+        System.out.println("---------Menu-------");
+          System.out.println("1.Registrar Estudiante");
+            System.out.println("0.Salir");
+                int opcion = entrada.nextInt();
+                if(opcion == 0 ){
+                    System.out.print("Saliendo....");
+                    System.exit(0);
+                }else if(opcion == 1){
+                    Estudiante estu = universidad.crearEstudiante();
+                    listaEstudiante.add(estu);
+                    System.out.print("El estudiante: "+estu.nombreCompleto+" Ha sido registrado.");
+                }
+                
+    }
     
 }
