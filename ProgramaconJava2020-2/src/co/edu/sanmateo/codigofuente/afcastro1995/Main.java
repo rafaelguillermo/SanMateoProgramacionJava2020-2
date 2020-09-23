972b8c1eb16566375c4c32977580da82989fa48e
felipe <felipe@DESKTOP-QA1308S>
2020-09-22 20:54:28 -0500

    Agregando Mi Main

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/afcastro1995/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.sanmateo.codigofuente.afcastro1995;
+
+/**
+ *
+ * @author felipe
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bienvenido Andres Felipe Castro Santos");
+    }
+    
+}
