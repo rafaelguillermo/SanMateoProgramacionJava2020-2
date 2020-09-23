df4ac594353e942b3aa9e880f36e45e836a92613
_Asap <_Asap@LR084F66S.red.ecopetrol.com.co>
2020-09-23 11:26:16 -0500

    Crear el menu

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Gersoncruz09/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Gersoncruz09/Main.java
@@ -5,10 +5,39 @@
  */
 package co.edu.sanmateo.codigofuente.Gersoncruz09;
 
+import co.edu.sanmateo.codigofuente.Gersoncruz09.modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.Gersoncruz09.operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author _Asap
  */
 public class Main {
+    
+    public static void main(String[]args){
+        
+        Scanner entrada = new Scanner(System.in);
+        List<Estudiante> listaEstudiante = new ArrayList<>();
+        Universidad universidad = new Universidad();
+        
+        while(true){
+            System.out.println("Bienvenido a la universidad San Mateo");
+            System.out.println("------ Menu ------");
+            System.out.println("1. Registrar Estudiante");
+            System.out.println("0. Sliar");
+            int opcion = entrada.nextInt();
+            if(opcion == 0) {
+                System.out.println("Saliendo...");
+                System.exit(0);
+            } else if (opcion == 1 ) {
+                Estudiante estu = universidad.crearEstudiante();
+                listaEstudiante.add(estu);
+                System.out.println("El estudiante: "+estu.nombreCompleto + "ha sido ingresado a al universidad");
+            }
+        }
+    }
     
 }
