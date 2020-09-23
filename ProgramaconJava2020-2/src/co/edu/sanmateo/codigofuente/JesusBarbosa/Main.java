b4c4d9143e84f64d142f96080b5f1298e2bacad9
LuckRotz <LuckRotz@DESKTOP-RHKO3D0>
2020-09-22 22:08:00 -0500

    Agregando nuevos Package

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
@@ -1,7 +1,35 @@
 package co.edu.sanmateo.codigofuente.JesusBarbosa;
 
+import co.edu.sanmateo.codigofuente.JesusBarbosa.modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.JesusBarbosa.operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Soy  Jesus Barbosa");
+
+        Scanner entrada = new Scanner(System.in);
+        List<Estudiante> listaEstudiante = new ArrayList<>();
+        Universidad universidad = new Universidad();
+
+        while (true) {
+            System.out.println("Bienvenido a la Fundación Universitaria San Mateo");
+            System.out.println("****** Menu de la Universidad ******");
+            System.out.println("Seleccione una de las siguintes opciones:");
+            System.out.println("1. Registrar Estudiante");
+            System.out.println("0. Salir");
+            int opcion = entrada.nextInt();
+            if (opcion == 0) {
+                System.out.println("Saliendo....");
+                System.exit(0);
+            } else if (opcion == 1) {
+                Estudiante estu = universidad.crearEstudiante();
+                listaEstudiante.add(estu);
+                System.out.println("El estudiante: " + estu.nombreCompleto + " ha sido ingresado a la universidad");
+            }
+        }
+
     }
 }
--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/modelo/Estudiante.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/modelo/Estudiante.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package co.edu.sanmateo.codigofuente.JesusBarbosa;
+package co.edu.sanmateo.codigofuente.JesusBarbosa.modelo;
 
 /**
  *
@@ -14,14 +9,9 @@ public class Estudiante {
     public String nombreCompleto;
     public String cedula;
 
-    public Estudiante() {
-    }
-
     public Estudiante(String nombreCompleto, String cedula) {
         this.nombreCompleto = nombreCompleto;
         this.cedula = cedula;
     }
-    
-    
     
 }
