2afa0c8646aabfcf90aa2d9c09fb823f80fe0934
Administrador <Administrador@DESKTOP-75R47M6>
2020-09-22 22:04:16 -0500

    Codigo Fuente

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Hugooxz/modelo/Estudiante.java
@@ -0,0 +1,14 @@
+
+package co.edu.sanmateo.codigofuente.Hugooxz.modelo;
+
+public class Estudiante {
+        public String nombreCompleto;
+    public String cedula;
+
+    public Estudiante(String nombreCompleto, String cedula) {
+        this.nombreCompleto = nombreCompleto;
+        this.cedula = cedula;
+    }
+    
+}
+    
---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Hugooxz/operaciones/Universidad.java
@@ -0,0 +1,28 @@
+
+package co.edu.sanmateo.codigofuente.Hugooxz.operaciones;
+import co.edu.sanmateo.codigofuente.Hugooxz.modelo.Estudiante;
+import java.util.Scanner;
+
+public class Universidad {
+
+    public Universidad() {
+    }
+    
+    
+    public Estudiante crearEstudiante(){
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("--------------------------------");
+        System.out.println("Ingresando un nuevo estudiante");
+        System.out.println("Nombre completo del estudiante");
+        String nombre = entrada.nextLine();
+        
+        System.out.println("Cedula del estudiante");
+        String cedula = entrada.nextLine();
+        
+        Estudiante estudiante = new Estudiante(nombre, cedula);
+        System.out.println("--------------------------------");
+        
+        return estudiante;
+    }
+}
