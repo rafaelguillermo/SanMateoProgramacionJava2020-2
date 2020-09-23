ec01fbb75fee5c47b54e806cd6776be7346ef111
jrangelardila <jrangelardila@gmaill.com>
2020-09-22 22:58:45 -0500

    Creando la opción Ingresar estudiante en el menú

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Main.java
@@ -5,6 +5,12 @@
  */
 package co.edu.sanmateo.codigofuente.jrangelardila;
 
+import co.edu.sanmateo.codigofuente.jrangelardila.modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.jrangelardila.operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author jrangelardila
@@ -16,7 +22,25 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       System.out.println("Hola");
+        List<Estudiante> listEstudents = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        Universidad university = new Universidad();
+
+        while (true) {
+            System.out.println("Bienvenidos a la Fundación Universitaria San Mateo\n"
+                    + "-----------------------MENÚ-----------------------\n"
+                    + "1- Registrar un nuevo estudiante\n0- Salir\n");
+            int option = input.nextInt();
+            if (option == 0) {
+                System.out.println("Saliendo......");
+                System.exit(0);
+            } else if (option == 1) {
+                Estudiante newStudent = university.newStudent();
+                listEstudents.add(newStudent);
+                System.out.println("\nEl estudiante " + newStudent.name + " ha sido registrado\n");
+            }
+        }
+
     }
-    
+
 }
