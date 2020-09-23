14ab93da4afea3a874b2d6b801568d0f6718250f
javierMY <javierMY@DESKTOP-UHE5N6N>
2020-09-22 22:04:22 -0500

    se a realizado la clase main

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Madrigaly5/Main.java
@@ -0,0 +1,15 @@
+
+package co.edu.sanmateo.codigofuente.Madrigaly5;
+
+/**
+ *
+ * @author javierMY
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la clase principal");
+    }
+    
+}
