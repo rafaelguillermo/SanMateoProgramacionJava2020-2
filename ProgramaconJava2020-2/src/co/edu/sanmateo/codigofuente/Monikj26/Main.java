591d5fca8222dd25aeb59f89e9930d8cd09546a8
monij <monij@DESKTOP-L943C1Q>
2020-09-23 20:42:15 -0500

    Actualización del ejercicio

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Monikj26/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Monikj26/Main.java
@@ -1,14 +1,37 @@
 
 package co.edu.sanmateo.codigofuente.Monikj26;
 
+import co.edu.sanmateo.codigofuente.Monikj26.modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.Monikj26.operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+                Scanner entrada = new Scanner(System.in);
+        List<Estudiante> listaEstudiante = new ArrayList<>();
+        Universidad universidad = new Universidad();
+
+        while (true) {
+            System.out.println("Bienvenido a la universidad San Mateo");
+            System.out.println("-------- Menu ------------");
+            System.out.println("1. Registrar Estudiante");
+            System.out.println("0. Salir");
+            int opcion = entrada.nextInt();
+            if (opcion == 0) {
+                System.out.println("Saliendo....");
+                System.exit(0);
+            } else if (opcion == 1) {
+                Estudiante estudiante = universidad.crearEstudiante();
+                listaEstudiante.add(estudiante);
+                System.out.println("El estudiante: " + estudiante.nombreCompleto + " ha sido ingresado a la universidad");
+            }
+        }
     }
     
 }
---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Monikj26/modelo/Estudiante.java
@@ -0,0 +1,14 @@
+
+package co.edu.sanmateo.codigofuente.Monikj26.modelo;
+
+
+public class Estudiante {
+    
+    public String nombreCompleto;
+    public String cedula;
+
+    public Estudiante(String nombreCompleto, String cedula) {
+        this.nombreCompleto = nombreCompleto;
+        this.cedula = cedula;
+    }
+}
