135902f49924fa495da9c93817193069c8f3ea95
monij <monij@DESKTOP-L943C1Q>
2020-09-23 20:26:22 -0500

    Practica mi clase Main

---
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Monikj26/Main.java
@@ -0,0 +1,14 @@
+
+package co.edu.sanmateo.codigofuente.Monikj26;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
