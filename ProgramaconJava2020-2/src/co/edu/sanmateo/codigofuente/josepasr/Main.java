21fc3a21b843fa5c933bfc377553b909d4282c3a
soporte <soporte@DESKTOP-6U4J9F6.fiscalia.col>
2020-09-26 12:45:13 -0500

    Creando sistema de ingreso de un estudiante

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/josepasr/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/josepasr/Main.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.sanmateo.codigofuente.josepasr;
 
-/**
- *
- * @author soporte
- */
+import co.edu.sanmateo.codigofuente.rafaelguillermo.modelo.Estudiante;
+import co.edu.sanmateo.codigofuente.rafaelguillermo.operaciones.Universidad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    
+public static void main (String[]args){
+    Scanner entrada= new Scanner (System.in);
+    List <Estudiante> listaEstudiante =new ArrayList<>();
+    Universidad universidad= new Universidad();
+    while (true){
+        System.out.println("Bienvenido A La Fundacion Universitaria San Mateo");
+        System.out.println("-------------Menu-------------");
+        System.out.println("1. Registrar Estudiante");
+        System.out.println("0. Salir");
+        int opcion= entrada.nextInt();
+        if(opcion==0){
+            System.out.println("Saliendo del proyecto............");
+            System.exit(0);
+                        
+        }else if (opcion==1) {
+            Estudiante estu=universidad.crearEstudiante();
+            listaEstudiante.add(estu);
+            System.out.println("El estudiante: "+ estu.nombreCompleto+ "Ha sido ingresado al sistema de la universidad");
+            
+        }
+        
+    }
+       
+}
 }
