6ed6004f96c423718f66974ff181079bf8311513
LuckRotz <LuckRotz@DESKTOP-RHKO3D0>
2020-09-25 21:26:57 -0500

    Actualizando clases

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/operaciones/Universidad.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/operaciones/Universidad.java
@@ -13,11 +13,11 @@ public class Universidad {
         
         Scanner entrada = new Scanner(System.in);
         System.out.println("--------------------------------");
-        System.out.println("Ingresando un nuevo estudiante");
-        System.out.println("Nombre completo del estudiante");
+        System.out.println("*****Ingresando un nuevo estudiante*****");
+        System.out.println("*****Nombre completo del estudiante*****");
         String nombre = entrada.nextLine();
         
-        System.out.println("Cedula del estudiante");
+        System.out.println("*****Cedula del estudiante*****");
         String cedula = entrada.nextLine();
         
         Estudiante estudiante = new Estudiante(nombre, cedula);
