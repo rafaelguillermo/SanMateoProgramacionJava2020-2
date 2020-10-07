968bc7a69d5429bbb8fcfb5a6482324594198019
duvan <duvan@192.168.1.60>
2020-10-07 14:58:25 -0500

    Creando opcion dos del menú la cual nos permite enlistar los estudiantes del sistema

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/Main.java
@@ -15,20 +15,35 @@ public class Main {
         List<Estudiante> listaEstudiante = new ArrayList<>();
         Universidad universidad = new Universidad();
 
-        System.out.println("Bienvenidos a la universidad San Mateo");
-        System.out.println("----------Menú----------");
-        System.out.println("1) Registrar estudiante");
-        System.out.println("0) Salir");
+        while (true) {
+            System.out.println("Bienvenidos a la universidad San Mateo");
+            System.out.println("----------Menú----------");
+            System.out.println("1) Registrar estudiante");
+            System.out.println("2) Listar estudiantes");
+            System.out.println("0) Salir");
 
-        int opcion = entrada.nextInt();
+            int opcion = entrada.nextInt();
 
-        if (opcion == 0) {
-            System.out.println("Saliendo...");
-            System.exit(0);
-        } else if (opcion == 1) {
-            Estudiante estu = universidad.crearEstudiante();
-            listaEstudiante.add(estu);
-            System.out.println("El estudiante: "+estu.nombreCompleto+" ha sido ingresado a la universidad");
+            if (opcion == 0) {
+                System.out.println("Saliendo...");
+                System.exit(0);
+            } else if (opcion == 1) {
+                Estudiante estu = universidad.crearEstudiante();
+                listaEstudiante.add(estu);
+                System.out.println("El estudiante: " + estu.nombreCompleto + " ha sido ingresado a la universidad");
+            } else if (opcion == 2) {
+                System.out.println("Los estudiantes registrados en el sistema son: ");
+                System.out.println("NOMBRE:                CEDULA:");
+                for (int i = 0; i < listaEstudiante.size(); i++) {
+                    Estudiante estudiante = listaEstudiante.get(i);
+                    System.out.println(estudiante.cedula + "               " + estudiante.nombreCompleto);
+                }
+                System.out.println("-------------------------------");
+                
+            } else if (opcion==3) {
+                System.out.println("Ingrese el usuario que esta buscando");
+                
+            }
         }
 
     }
