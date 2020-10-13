d32d52967db21fff071d6a6c28674e123f8ddc2a
Sistemas <Sistemas@DESKTOP-K5QMM82>
2020-10-13 15:52:34 -0500

    Creación de una lista en el menú de la Fundación Universitaria San Mateo.

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/YersonRomero/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/YersonRomero/Main.java
@@ -19,22 +19,39 @@ public class Main {
  public static void main (String[] args){
      
      Scanner entrada = new Scanner(System.in);
-     List<Estudiante> listaEstudiante = new ArrayList<>();
+     List<Estudiante> listaEstudiante = new ArrayList<>(); //Creación de lista
      Universidad universidad = new Universidad();
      
         while (true) {
-            System.out.println("\033[35mBienvenidos a la Fundación Universitaria San Mateo");
-            System.out.println("\033[34m------ Menú ------");
-            System.out.println("\033[32m1. Registrar Estudiante");
-            System.out.println("\033[31m0. Salir");
+            System.out.println("|--------------------------------------------------|");
+            System.out.println("|Bienvenidos a la Fundación Universitaria San Mateo|");
+            System.out.println("|---------------------- Menú ----------------------|");
+            System.out.println("|1. Registrar Estudiante                           |");
+            System.out.println("|2. Listar Estudiantes                             |");
+            System.out.println("|0. Salir                                          |");
+            System.out.println("|--------------------------------------------------|");
+            
             int opcion = entrada.nextInt();
+            
             if (opcion == 0) {
-                System.out.println("\033[31mSaliendo...");
+                System.out.println("Saliendo...");
                 System.exit(0);
             } else if (opcion == 1) {
                 Estudiante estu = universidad.crearEstudiante();
-                listaEstudiante.add(estu);
-                System.out.println("\033[36mEl estudiante " + estu.nombreCompleto + " ha sido ingresado a la iniversidad");
+                listaEstudiante.add(estu); //El .add es para adicionar un estudiante a la lista.
+                System.out.println("El estudiante " + estu.nombreCompleto + " ha sido ingresado a la iniversidad");
+            } else if (opcion == 2){
+                //Un ciclo for realiza tres operaciones fundamentales de la siguiente manera:
+                //1. Inicialización.
+                //2. Pregunta.
+                //3. Incremnta o Disminuye.
+                System.out.println("ESTUDIANTES A MOSTRAR: "+listaEstudiante.size()); //El .size es para conocer el tamaño de la lista.
+                System.out.println("DOCUMENTO      NOMBRE COMPLETO");
+                System.out.println("|--------------------------------------------------|");
+                for (int indice=0; indice<listaEstudiante.size(); indice++){
+                    Estudiante estudiante = listaEstudiante.get(indice); //El .get es para mostrar la información de un estudiante.
+                    System.out.println(estudiante.cedula+"      "+estudiante.nombreCompleto);
+                }
             }
         }
     }
