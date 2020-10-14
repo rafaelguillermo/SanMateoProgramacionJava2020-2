20ba55b595c81028379f1af0bb41b1c9d0e4cb9a
Rafael Blanco <mangelan03@gmail.com>
2020-10-13 21:14:29 -0500

    Menu opcion 3

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/rafaelguillermo/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/rafaelguillermo/Main.java
@@ -6,20 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Rafael Guillermo Blanco Banquez <rafaelg.blancob@gmail.com>
+ */
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
         List<Estudiante> listaEstudiante = new ArrayList<>();
+        
         Universidad universidad = new Universidad();
 
         while (true) {
-            System.out.println("Bienvenido a la universidad San Mateo");
+            
+            System.out.println("Bienvenido a la universidad San Mateo > RAFAEL");
             System.out.println("-------- Menu ------------");
             System.out.println("1. Registrar Estudiante");
+            System.out.println("2. Listar Estudiantes");
+            System.out.println("3. Buscar Estudiante en la lista");
             System.out.println("0. Salir");
+            
             int opcion = entrada.nextInt();
+            
             if (opcion == 0) {
                 System.out.println("Saliendo....");
                 System.exit(0);
@@ -27,6 +37,33 @@ public class Main {
                 Estudiante estu = universidad.crearEstudiante();
                 listaEstudiante.add(estu);
                 System.out.println("El estudiante: " + estu.nombreCompleto + " ha sido ingresado a la universidad");
+            }else if (opcion == 2) {
+                //Inicializacion
+                //Pregunta
+                //Incremento
+                System.out.println("ESTUDIANTES A MOSTRAR: "+listaEstudiante.size());
+                System.out.println("DOCUMENTO       NOMBRE COMPLETO");
+                System.out.println("---------------------------------");
+                
+                for(int indice=0; indice < listaEstudiante.size(); indice ++ ){
+                    
+                    Estudiante estudiante = listaEstudiante.get(indice);
+                    System.out.println(estudiante.cedula+"      "+estudiante.nombreCompleto);
+                }
+            }else if(opcion == 3){
+                System.out.println("Por ingrese la identificación a buscar: ");
+                String identificacion;
+                identificacion = entrada.next();
+                
+                for(int indice=0; indice < listaEstudiante.size(); indice ++ ){
+                    
+                    Estudiante estudiante = listaEstudiante.get(indice);
+                    
+                    if(estudiante.cedula.equals(identificacion)){
+                        System.out.println("ESTUDIANTE ENCONTRADO..."+estudiante.nombreCompleto);
+                    }
+                    //System.out.println(estudiante.cedula+"      "+estudiante.nombreCompleto);
+                }
             }
         }
 
