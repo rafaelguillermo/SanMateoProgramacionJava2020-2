f53868c8d094590bea99b8058ff114011d3b5969
LuckRotz <LuckRotz@DESKTOP-RHKO3D0>
2020-10-19 20:37:29 -0500

    Escribir y leer un .txt

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/JesusBarbosa/Main.java
@@ -2,13 +2,19 @@ package co.edu.sanmateo.codigofuente.JesusBarbosa;
 
 import co.edu.sanmateo.codigofuente.JesusBarbosa.modelo.Estudiante;
 import co.edu.sanmateo.codigofuente.JesusBarbosa.operaciones.Universidad;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public void mostrarMenu() throws IOException {
 
         Scanner entrada = new Scanner(System.in);
         List<Estudiante> listaEstudiante = new ArrayList<>();
@@ -21,56 +27,82 @@ public class Main {
             System.out.println("1. Registrar Estudiante");
             System.out.println("2. Listar Estudiantes");
             System.out.println("3. Buscar estudiante (documento)");
+            System.out.println("4. Guardar información de los estudiantes");
             System.out.println("0. Salir");
             System.out.println("-----------------------");
             int opcion = entrada.nextInt();
             if (opcion == 0) {
                 System.out.println("Saliendo....");
                 System.exit(0);
-            } 
-            else if (opcion == 1) {
+            } else if (opcion == 1) {
                 Estudiante estudiantes = universidad.crearEstudiante();
                 listaEstudiante.add(estudiantes);
                 System.out.println("El estudiante: " + estudiantes.nombreCompleto + " ha sido ingresado a la universidad");
                 System.out.println("-------------------------------------");
-            } 
-            else if (opcion == 2){
+            } else if (opcion == 2) {
                 //Inicializar la cantidad de estudiantes
                 //Pregunta 
                 //Incremento
-                
-                System.out.println("Los estudiantes a mostrar son: "+ listaEstudiante.size());
+
+                System.out.println("Los estudiantes a mostrar son: " + listaEstudiante.size());
                 System.out.println("DOCUMENTO                       NOMBRE");
                 System.out.println("----------------------------------------------------------");
-                
+
                 //Inicia el For que iniciara el recorrido imprimiendo todos los estudiantes de la lista
-                
-                for(int indice = 0;indice < listaEstudiante.size(); indice++){
+                for (int indice = 0; indice < listaEstudiante.size(); indice++) {
                     Estudiante estudiante = listaEstudiante.get(indice);
-                    System.out.println(estudiante.cedula+"                     "+estudiante.nombreCompleto);                 
+                    System.out.println(estudiante.cedula + "                     " + estudiante.nombreCompleto);
                 }
                 System.out.println("----------------------------------------------------------");
-            }
-            else if(opcion == 3){
+            } else if (opcion == 3) {
                 System.out.println("Escriba la cedula del estudiante a buscar");
                 System.out.println("-------------------------");
                 String docBuscar = entrada.next();
                 int existe = 0;
-                for(int indice = 0;indice < listaEstudiante.size(); indice++){
+                for (int indice = 0; indice < listaEstudiante.size(); indice++) {
                     Estudiante estudiante = listaEstudiante.get(indice);
-                    if(estudiante.cedula.equals(docBuscar)){
+                    if (estudiante.cedula.equals(docBuscar)) {
                         System.out.println("El estudiante si existe en la lista");
-                        System.out.println(estudiante.nombreCompleto+"       "+estudiante.cedula);
+                        System.out.println(estudiante.nombreCompleto + "       " + estudiante.cedula);
                         System.out.println("------------------------------");
                         existe = 1;
                     }
                 }
-                if(existe == 0){
-                    System.out.println("El estudiante con documento "+docBuscar+" no existe en la lista");
+                if (existe == 0) {
+                    System.out.println("El estudiante con documento " + docBuscar + " no existe en la lista");
                     System.out.println("------------------------------");
                 }
+
+            } else if (opcion == 4) {
+                //System.out.println("");
+                FileWriter fw = new FileWriter("C:\\Users\\LuckRotz\\Documents\\NetBeansProjects\\Base\\estudiantes2.txt", false);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                for (int indice = 0; indice < listaEstudiante.size(); indice++) {
+
+                    Estudiante estudiante = listaEstudiante.get(indice);
+                    bw.write(estudiante.cedula + "," + estudiante.nombreCompleto);
+                    bw.write("\n");
+                }
+                fw.close();
+                System.out.println("Los estudiantes han sido guardados correctamente.");
+
+            } else if (opcion == 5) {
+                FileReader fr = new FileReader("C:\\Users\\LuckRotz\\Documents\\NetBeansProjects\\Base\\estudiantes2.txt");
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    System.out.println(linea);
+                }
             }
+
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main m = new Main();
+        m.mostrarMenu();
 
     }
 }
