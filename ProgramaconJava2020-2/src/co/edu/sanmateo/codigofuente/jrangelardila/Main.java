109680789744e71db437e7d3f6292f42637dc76b
jrangelardila <jrangelardila@gmaill.com>
2020-10-23 00:42:43 -0500

    Crear estudiantes a partir del archivo txt

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Main.java
@@ -2,30 +2,57 @@ package co.edu.sanmateo.codigofuente.jrangelardila;
 
 import co.edu.sanmateo.codigofuente.jrangelardila.modelo.Estudiante;
 import co.edu.sanmateo.codigofuente.jrangelardila.operaciones.Universidad;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /**
  *
  * @author jrangelardila
  */
 public class Main {
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public void menu() {
         List<Estudiante> listEstudents = new ArrayList<>();
+
+        //traer estudiantes guardados
+        try {
+            File fileSaved = new File("/home/jrangelardila/NetBeansProjects/SanMateoProgramacionJava2020-2/SanMateoProgramacionJava2020-2/ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Students.txt");
+            Scanner readFile = new Scanner(fileSaved);
+            while (readFile.hasNextLine()) {
+                String line = readFile.nextLine();
+                String identification = "";
+                String name = "";
+                boolean coma = false;
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) != ',' && coma == false) {
+                        identification = identification + line.charAt(i);
+                    } else if (line.charAt(i) == ',' && coma == false) {
+                        coma = true;
+                    }
+                    if (line.charAt(i) != ',' && coma == true) {
+                        name = name + line.charAt(i);
+                    }
+                }
+                Estudiante student = new Estudiante(name, identification);
+                listEstudents.add(student);
+            }
+
+        } catch (Exception e) {
+            System.out.println("\n El archivo no existe: " + e);
+        }
+
         Scanner input = new Scanner(System.in);
         Universidad university = new Universidad();
         while (true) {
-            System.out.println("Bienvenidos a la Fundación Universitaria San Mateo\n"
+            System.out.println("\nBienvenidos a la Fundación Universitaria San Mateo\n"
                     + "-----------------------MENÚ-----------------------\n"
                     + "1- Registrar un nuevo estudiante\n"
                     + "2- Listar estudiantes\n"
                     + "3- Buscar estudiantes en la lista\n"
-                    + "4- "
+                    + "4- Guardar estudiantes\n"
                     + "0- Salir\n");
             int option = input.nextInt();
             if (option == 0) {
@@ -35,33 +62,52 @@ public class Main {
                 Estudiante newStudent = university.newStudent();
                 listEstudents.add(newStudent);
                 System.out.println("\nEl estudiante " + newStudent.name + " ha sido registrado\n");
-            } else if(option ==2){
-                System.out.println("ESTUDIANTES A MOSTRAR "+ listEstudents.size());
+            } else if (option == 2) {
+                System.out.println("ESTUDIANTES A MOSTRAR " + listEstudents.size());
                 for (int i = 0; i < listEstudents.size(); i++) {
                     Estudiante estudiante = listEstudents.get(i);
                     System.out.println(estudiante.identification + "      " + estudiante.name);
                 }
-            } else if(option ==3){
+            } else if (option == 3) {
                 System.out.println("Ingresar el numero de indentificación a buscar:\n");
-                String identificactionSearch=input.next();
-                boolean search=false;
-               Estudiante newStudent=null;
-                for(int i=0;i<listEstudents.size();i++){
-                     newStudent=listEstudents.get(i);
-                    if(newStudent.identification.equals(identificactionSearch)){
-                        search=true;
-                        i=listEstudents.size();
+                String identificactionSearch = input.next();
+                boolean search = false;
+                Estudiante newStudent = null;
+                for (int i = 0; i < listEstudents.size(); i++) {
+                    newStudent = listEstudents.get(i);
+                    if (newStudent.identification.equals(identificactionSearch)) {
+                        search = true;
+                        i = listEstudents.size();
                     }
                 }
-                
-               if(search==true){
-                    System.out.println("Estudiante identificado con "+identificactionSearch+" ha sido encontrado: "+newStudent.name+"\n");
-               }else{
-                   System.out.println("Estudiante identificado con "+identificactionSearch+" no hs sido encontrado\n");
-               }
+
+                if (search == true) {
+                    System.out.println("Estudiante identificado con " + identificactionSearch + " ha sido encontrado: " + newStudent.name + "\n");
+                } else {
+                    System.out.println("Estudiante identificado con " + identificactionSearch + " no hs sido encontrado\n");
+                }
+            } else if (option == 4) {
+                try {
+                    FileWriter fileWriter = new FileWriter("/home/jrangelardila/NetBeansProjects/SanMateoProgramacionJava2020-2/SanMateoProgramacionJava2020-2/ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jrangelardila/Students.txt", false);
+                    BufferedWriter file = new BufferedWriter(fileWriter);
+                    for (int i = 0; i < listEstudents.size(); i++) {
+                        Estudiante estudiante = listEstudents.get(i);
+                        file.write(estudiante.identification + "," + estudiante.name + "\n");
+                    }
+                    System.out.println("\nLos estudintes han sido guardados");
+                    file.close();
+                } catch (Exception e) {
+                    System.out.println("\nLa ubicación es erronea: " + e);
+                }
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main main = new Main();
+        main.menu();
     }
 
 }
