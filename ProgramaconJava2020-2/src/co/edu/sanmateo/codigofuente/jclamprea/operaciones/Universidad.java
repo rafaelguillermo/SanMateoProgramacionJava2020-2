5929c9e1691f7bbfbab2662bd51e34ed553a4ee5
portatil <portatil@DESKTOP-DQUDPPA>
2020-10-26 20:46:58 -0500

    Se crea metodo para buscar estudiantes y reutilizarlo para la eliminación de un estudiante

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jclamprea/operaciones/Universidad.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/jclamprea/operaciones/Universidad.java
@@ -6,6 +6,7 @@
 package co.edu.sanmateo.codigofuente.jclamprea.operaciones;
 
 import co.edu.sanmateo.codigofuente.jclamprea.modelo.Estudiante;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,23 @@ public class Universidad {
 
     public Universidad() {
     }
-    
+    public Estudiante buscarEstudiante(Scanner entrada, List<Estudiante> listaEstudiante){
+        
+        System.out.println("Por ingrese la identificación a buscar: ");
+        String identificacion;
+        identificacion = entrada.next();
+
+        for(int indice=0; indice < listaEstudiante.size(); indice ++ ){
+
+            Estudiante estudiante = listaEstudiante.get(indice);
+
+            if(estudiante.cedula.equals(identificacion)){
+                return estudiante;
+            }
+            //System.out.println(estudiante.cedula+"      "+estudiante.nombreCompleto);
+        }
+        return null;
+    }
     
     public Estudiante crearEstudiante(){
         
