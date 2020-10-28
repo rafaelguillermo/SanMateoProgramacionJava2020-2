5d064991f9ba8a6bdf827a48b771f48fcabd850c
duvan <duvan@192.168.1.60>
2020-10-27 22:22:49 -0500

    Creando opcion tres del ménu (Buscar estudiante)

--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/Main.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/Main.java
@@ -2,7 +2,6 @@ package co.edu.sanmateo.codigofuente.Duvanar;
 
 import co.edu.sanmateo.codigofuente.Duvanar.modelo.Estudiante;
 import co.edu.sanmateo.codigofuente.Duvanar.operaciones.Universidad;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
@@ -16,10 +15,12 @@ public class Main {
         Universidad universidad = new Universidad();
 
         while (true) {
+            System.out.println("------------------------");
             System.out.println("Bienvenidos a la universidad San Mateo");
             System.out.println("----------Menú----------");
             System.out.println("1) Registrar estudiante");
             System.out.println("2) Listar estudiantes");
+            System.out.println("3) Buscar estudiante");
             System.out.println("0) Salir");
 
             int opcion = entrada.nextInt();
@@ -27,10 +28,12 @@ public class Main {
             if (opcion == 0) {
                 System.out.println("Saliendo...");
                 System.exit(0);
+
             } else if (opcion == 1) {
                 Estudiante estu = universidad.crearEstudiante();
                 listaEstudiante.add(estu);
                 System.out.println("El estudiante: " + estu.nombreCompleto + " ha sido ingresado a la universidad");
+
             } else if (opcion == 2) {
                 System.out.println("Los estudiantes registrados en el sistema son: ");
                 System.out.println("NOMBRE:                CEDULA:");
@@ -40,9 +43,15 @@ public class Main {
                 }
                 System.out.println("-------------------------------");
                 
-            } else if (opcion==3) {
-                System.out.println("Ingrese el usuario que esta buscando");
-                
+            } else if (opcion == 3) {
+                System.out.println("Ingrese el numero de identificacións del estudiante");
+                String identificacion =entrada.next();
+                for (int i = 0; i < listaEstudiante.size(); i++) {
+                     Estudiante estudiante = listaEstudiante.get(i);
+                     if (estudiante.cedula.equals(identificacion)) {
+                         System.out.println("Estudiante encontrado: "+estudiante.nombreCompleto);
+                    }
+                }
             }
         }
 
--- ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/operaciones/Universidad.java
+++ ProgramaconJava2020-2/src/co/edu/sanmateo/codigofuente/Duvanar/operaciones/Universidad.java
@@ -3,22 +3,18 @@ package co.edu.sanmateo.codigofuente.Duvanar.operaciones;
 import co.edu.sanmateo.codigofuente.Duvanar.modelo.Estudiante;
 import java.util.Scanner;
 
-/**
- *
- * @author duvan
- */
 public class Universidad {
 
     public Universidad() {
     }
 
-    
-    public Estudiante crearEstudiante(){
+    public Estudiante crearEstudiante() {
 
         Scanner entrada = new Scanner(System.in);
-        System.out.println("--------------------------------");
+        System.out.println("------------------------------");
         System.out.println("Ingresando un nuevo estudiante");
-        System.out.println("Nombre completo del estudiante");
+        System.out.println("------------------------------");
+        System.out.println("Ingrese el nombre completo del estudiante");
         String nombre = entrada.nextLine();
 
         System.out.println("Ingrese la cedula del estudiante");
